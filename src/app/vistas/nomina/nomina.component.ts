af00715139919ed8c86bfba81f620ce107a74a55
josselynVal <josselyn.valenciap@gmail.com>
2021-11-08 05:25:28 -0600

    Reportes pdf

--- src/app/vistas/nomina/nomina.component.ts
+++ src/app/vistas/nomina/nomina.component.ts
@@ -10,6 +10,11 @@ import { element } from 'protractor';
 import { PropertyRead } from '@angular/compiler';
 import Swal from 'sweetalert2';
 
+//pdf
+import pdfMake from 'pdfmake/build/pdfmake'; 
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-nomina',
@@ -33,6 +38,70 @@ export class NominaComponent implements OnInit {
     isss:string;
     contratacion:string;
     sueldo:number;
+
+    /*createPdf (){
+      //const pdfDefinition: any = {content:[{text:'hola'}]}
+      const pdfDefinition: any = {content:[{
+
+      }]}
+      const pdf = pdfMake.createPdf (pdfDefinition);
+      pdf.open();
+    }*/
+
+    createPdf (){
+      //const pdfDefinition: any = {content:[{text:'hola'}]}
+      const pdfDefinition: any = {content:[{
+        //text:'hola'
+        
+      table: {
+          body: [  
+            ['Nombre', 'DUI', 'Cargo', 'Horas trabajadas', 'AFP', 'ISSS', 'Sueldo'],  
+            ...this.empleadoList.map(p => ([p.nombre, p.dui, p.cargo,p.horas,p.afp,p.isss,p.sueldo]))  
+        ] 
+        }
+
+      }]}
+      const pdf = pdfMake.createPdf (pdfDefinition);
+      pdf.open();
+    }
+
+/*    createPdf2 (){
+      //const pdfDefinition: any = {content:[{text:'hola'}]}
+      const pdfDefinition: any = {content:[{
+        //text:'hola'
+        
+      table: {
+          body: [  
+            ['Nombre', 'DUI', 'Cargo', 'Horas trabajadas', 'AFP', 'ISSS', 'Sueldo'],  
+            ...this.empleadoList.map(p => ([p.nombre, p.dui, p.cargo,p.horas,p.afp,p.isss,p.sueldo]))  
+        ] 
+        }
+
+      }]}
+      const pdf = pdfMake.createPdf (pdfDefinition);
+      pdf.open();
+    }
+*/
+  /* generatePDF(action = 'open') {    
+        
+      let docDefinition = {      
+        content :[{
+          text: 'Order Details',  
+          style: 'sectionHeader'
+        },
+        {
+          table: {  
+            headerRows: 1,  
+            widths: ['*', 'auto', 'auto', 'auto'],  
+            body: [  
+                ['Nombre', 'DUI', 'Cargo', 'Horas trabajadas', 'AFP', 'ISSS', 'Sueldo'],  
+                ...this.empleadoList.map(p => ([p.nombre, p.dui, p.cargo,p.horas,p.afp,p.isss,p.sueldo]))  
+            ] 
+        }   
+        }
+      ],
+      }
+    }*/
   
   constructor(    
     public empleadoService: EmpleadoService
